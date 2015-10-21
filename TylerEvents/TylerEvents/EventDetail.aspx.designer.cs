8b4496c3702bba558933157e192c8e4c6855f607
gerdescm <gerdescm@u.washington.edu>
2015-10-21 13:38:23 -0700

    Added button showing/hiding and delete button

--- TylerEvents/TylerEvents/EventDetail.aspx.designer.cs
+++ TylerEvents/TylerEvents/EventDetail.aspx.designer.cs
@@ -157,6 +157,15 @@ namespace TylerEvents {
         protected global::System.Web.UI.WebControls.Button SaveEvent;
         
         /// <summary>
+        /// DeleteEvent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button DeleteEvent;
+        
+        /// <summary>
         /// ChatPanel control.
         /// </summary>
         /// <remarks>
